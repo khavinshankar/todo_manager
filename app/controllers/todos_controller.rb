b0a6ea5baf4b422a531ef49e603781fc55b4c6e8
khavinshankar <khavinshankar@gmail.com>
2021-01-17 14:55:13 +0530

    added /todos/create/ route to create todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
   end
@@ -6,5 +8,12 @@ class TodosController < ApplicationController
   def show
     id = params[:id]
     render plain: Todo.find(id).to_displayable_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    render plain: "Todo created with id, #{todo.id}"
   end
 end
