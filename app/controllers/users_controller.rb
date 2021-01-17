9816f7307c3466f35ad9ad2cbab01c9b210c854a
khavinshankar <khavinshankar@gmail.com>
2021-01-17 15:48:51 +0530

    added functionality to create a new user at post users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: User.all.map { |user| user.to_displayable_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    passwd = params[:password]
+    user = User.create!(name: name, email: email, password: passwd)
+    render plain: "hey #{name}, user successfully created with an id, #{user.id}"
   end
 end
