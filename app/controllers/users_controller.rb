86e76b4593fcdd6172c637fd2c4d8a4300d626e1
khavinshankar <khavinshankar@gmail.com>
2021-01-20 12:06:09 +0530

    added root page and sign up page with basic functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,17 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def index
-    render plain: User.all.map { |user| user.to_displayable_string }.join("\n")
+  def new
+    render "users/new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    passwd = params[:password]
-    user = User.create!(name: name, email: email, password: passwd)
-    render plain: "hey #{name}, user successfully created with an id, #{user.id}"
-  end
-
-  def login
-    email = params[:email]
-    passwd = params[:password]
-    user = User.find_by "email = ?", email
-    if !user
-      render plain: "User #{email} not found"
-    elsif passwd == user.password
-      render plain: "true"
-    else
-      render plain: "false"
-    end
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password_digest: params[:password],
+    )
+    redirect_to "/"
   end
 end
