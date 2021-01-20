35189fb943587c7951acebb05eba27d53b739e0d
khavinshankar <khavinshankar@gmail.com>
2021-01-20 10:11:46 +0530

    updated users table schema using active record migrations

---
+++ db/migrate/20210120043722_add_first_name_to_users.rb
@@ -0,0 +1,9 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :password_digest, :string
+    remove_column :users, :name, :string
+    remove_column :users, :password, :string
+  end
+end
